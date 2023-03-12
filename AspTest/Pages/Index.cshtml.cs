a7f98952f3ab29cfbbf3ec50aa8d1ec146c705b2
Jozef Šajtlava <xsajtlav@fi.muni.cz>
2023-03-13 00:08:18 +0100

    initial commit

---
+++ AspTest/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using AspTest.Models;
+using AspTest.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        //Logging is a service made available by ASP.NET
+        //If I want to log to Firewall or Azure, I dont make a logger, I ask for one
+        public IndexModel(ILogger<IndexModel> logger,
+                          JsonFileProductService productService)
+        {
+
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+
+        }
+    }
+}
---
+++ AspTest/Services/JsonFileProductService.cs
@@ -0,0 +1,31 @@
+﻿using AspTest.Models;
+using System.Text.Json;
+
+namespace AspTest.Services
+{
+    public class JsonFileProductService
+    {
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+
+        }
+
+    }
+}
